f2b4b947efac399193cc9b6cd5cf9f20dc32d92d
Harold Seo <harold.seo.612@gmail.com>
2022-03-29 17:33:17 -0700

    Update App and Navbar CSS, Install React Icons

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { BiChurch } from "react-icons/bi";
 
 import "./Navbar.css";
 
@@ -14,7 +15,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo'>
-            The Beacon
+            <BiChurch />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
